af6efb249028a9d481d53d4efd0854ea37616c30
elvis-visi <sivisevi1@gmail.com>
2024-01-18 11:46:05 +0100

    unicafe 1.7.1

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -4,6 +4,24 @@ const Button = ({ handler, text }) => {
   return <button onClick={handler}> {text}</button>;
 };
 
+const Statistics = ({ good, neutral, bad }) => {
+  const total = good + neutral + bad;
+  const average = total === 0 ? 0 : (good - bad) / total;
+  const positivePercentage = total === 0 ? 0 : (good / total) * 100;
+
+  return (
+    <div>
+      <h3>Statistics</h3>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {total}</p>
+      <p>average {average} </p>
+      <p>positive {positivePercentage} % </p>
+    </div>
+  );
+};
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0);
@@ -20,22 +38,13 @@ const App = () => {
     setBad(bad + 1);
   };
 
-  const total = good + bad + neutral;
-
   return (
     <div>
       <h2>give feedback</h2>
       <Button handler={handleGood} text="good" />
       <Button handler={handleNeutral} text="neutral" />
       <Button handler={handleBad} text="bad" />
-
-      <h3>Statistics</h3>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {total}</p>
-      <p>average {(good - bad) / total}</p>
-      <p>positive {(good / total) * 100} % </p>
+      <Statistics good={good} bad={bad} neutral={neutral} />
     </div>
   );
 };
