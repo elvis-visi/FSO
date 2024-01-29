ba9bb2dc9ff9c3d373256d02ff435c171a4ae14c
elvis-visi <sivisevi1@gmail.com>
2024-01-29 15:33:31 +0100

    app.js to create express app, connect to the DB, add the middleware and routes to app.

--- bloglist/index.js
+++ bloglist/index.js
@@ -1,23 +1,7 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
+const app = require('./app')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 
-const blogsRouter = require('../bloglist/controllers/blogs')
-
-const mongoUrl = 'mongodb+srv://fullstack:fullstack@cluster0.xjms7ej.mongodb.net/blogListApp?retryWrites=true&w=majority'
-mongoose.connect(mongoUrl)
-
-app.use(cors())
-app.use(express.json())
-
-app.use('/api/blogs', blogsRouter)
-
-
-
-
 app.listen(config.PORT, () => {
-  console.log(`Server running on port ${config.PORT}`)
+  logger.info(`Server running on port ${config.PORT}`)
 })
