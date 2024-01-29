266a7aceb66071a1bf58ac4748de801709661ae8
elvis-visi <sivisevi1@gmail.com>
2024-01-29 13:25:16 +0100

    blogRouter will handle requests

--- bloglist/index.js
+++ bloglist/index.js
@@ -3,7 +3,7 @@ const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 
-const Blog = require('../bloglist/models/blog')
+const blogsRouter = require('../bloglist/controllers/blogs')
 
 const mongoUrl = 'mongodb+srv://fullstack:fullstack@cluster0.xjms7ej.mongodb.net/blogListApp?retryWrites=true&w=majority'
 mongoose.connect(mongoUrl)
@@ -11,30 +11,9 @@ mongoose.connect(mongoUrl)
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
+app.use('/api/blogs', blogsRouter)
 
-app.post('/api/blogs', (request, response) => {
-  const body = request.body
-  
-  const blog = new Blog({
-    title: body.title,
-  author: body.author,
-  url: body.url,
-  likes: body.likes
-  })
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
 
 const PORT = 3003
 app.listen(PORT, () => {
