a58a560b3ce814c6521560830e649a9e30fcf5dd
elvis-visi <sivisevi1@gmail.com>
2024-02-01 12:12:46 +0100

    Update blog

--- bloglist/controllers/blogs.js
+++ bloglist/controllers/blogs.js
@@ -36,7 +36,24 @@ blogsRouter.get('/', async (request, response) => {
     response.status(204).end()
   })
 
+  blogsRouter.put('/:id', async (request,response,next) => {
+    const body = request.body  //new content 
 
+    const blog = {
+      title: body.title,
+      author: body.author,
+      url: body.url,
+      likes: body.likes
+    }
+
+    try{ 
+      const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {new: true})
+      response.json(updatedBlog)
+    }catch(Exception){
+      next(Exception)
+    }
+
+  })
 
 
 
