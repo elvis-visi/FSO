72de66882b59248dcd39c09e475e8da6ee3c2cb3
elvis-visi <sivisevi1@gmail.com>
2024-02-10 12:16:21 +0100

    added a button to each blog, which controls whether all of the details about the blog are shown or not

--- bloglist-frontend/src/components/Blog.jsx
+++ bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,45 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
+import { useState } from "react"
+
+const Blog = ({ blog }) =>  {
+
+  const [visible, setVisible] = useState(false)
+
+  //initially display ''
+  const hideWhenVisible = {display: visible ? 'none' : '' }
+    //initially display none
+  const showWhenVisible = { display: visible ? '' : 'none' }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  return (
+ <>
+  <div style={{...blogStyle, ...hideWhenVisible}}>
+    {blog.title} {blog.author}  <button onClick={toggleVisibility}>View</button>
   </div>  
-)
+  <div style={{...blogStyle, ...showWhenVisible}}>
+    <p>{blog.title} {blog.author}  <button onClick={toggleVisibility}>hide</button></p>
+    <p>{blog.url}</p>
+    <p>likes {blog.likes} <button>like</button></p>
+    <p>{blog.author}</p>
+  </div>
+ </>
+ 
+  )
+
+}
+
+
+
+
 
 export default Blog
