47c1bb9ed7cf01d7e206b821b22f35d91df144d7
elvis-visi <sivisevi1@gmail.com>
2024-03-08 11:00:41 +0100

    Structuring the app

--- part13/notes-backend/index.js
+++ part13/notes-backend/index.js
@@ -1,85 +1,25 @@
-require('dotenv').config()
-const { Sequelize, Model, DataTypes } = require('sequelize')
+// Express application -> backend server
 const express = require('express')
 const app = express()
+
+// import PORT on which we will listen for http requests on
+// DB connection
+const { PORT } = require('./util/config')
+const { connectToDatabase } = require('./util/db')
+
+//controller (mini-app) which will handle the requests to /api/notes
+const notesRouter = require('./controllers/notes')
+
 app.use(express.json())
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialectOptions: {
-    // ssl: {
-    //   require: true,
-    //   rejectUnauthorized: false
-    // }
-  },
-});
+app.use('/api/notes', notesRouter)
 
-class Note extends Model {}
-Note.init({
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true
-  },
-  content: {
-    type: DataTypes.TEXT,
-    allowNull: false
-  },
-  important: {
-    type: DataTypes.BOOLEAN
-  },
-  date: {
-    type: DataTypes.DATE
-  }
-}, {
-  sequelize,
-  underscored: true,
-  timestamps: false,
-  modelName: 'note'
-})
-
-Note.sync()
-
-
-
-app.get('/api/notes', async (req, res) => {
-    const notes = await Note.findAll()
-    res.json(notes)
-  })
-
-
-
-app.post('/api/notes', async (req, res) => {
-    console.log(req.body); 
-    try {
-    const note = await Note.create(req.body)
-    return res.json(note)
-  } catch (error) {
-    return res.status(400).json({ error })
-  }
-})
-
-app.get('/api/notes/:id', async (req, res) => {
-  const note = await Note.findByPk(req.params.id)
-  if (note) {
-    res.json(note)
-  } else {
-    res.status(404).end()
-  }
-})
-
-app.put('/api/notes/:id', async (req, res) => {
-  const note = await Note.findByPk(req.params.id)
-  if (note) {
-    note.important = req.body.important
-    await note.save()
-    res.json(note)
-  } else {
-    res.status(404).end()
-  }
-})
-
-  const PORT = process.env.PORT || 3001
+//wait for the connection to DB to be established, then start the server
+const start = async () => {
+  await connectToDatabase()
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
   })
+}
 
+start()
