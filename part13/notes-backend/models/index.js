97a3ad210ff22f0160a7f8af1f90ebae1bafb8a8
elvis-visi <sivisevi1@gmail.com>
2024-03-08 11:48:29 +0100

    Connection between users and notes, 1-M relationship

--- part13/notes-backend/models/index.js
+++ part13/notes-backend/models/index.js
@@ -1,8 +1,11 @@
 const Note = require('./note')
 const User = require('./user')
 
-Note.sync()
-User.sync()
+// 1-M relationship connection between the users and notes entries. 
+User.hasMany(Note)
+Note.belongsTo(User)
+Note.sync({ alter: true })
+User.sync({ alter: true })
 
 module.exports = {
   Note, User
