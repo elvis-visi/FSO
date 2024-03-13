6272f6a51f9538e931aa739477dfa350ef4cafd4
elvis-visi <sivisevi1@gmail.com>
2024-03-13 20:02:14 +0100

    Added migration rollback; revert the previous migration

--- part13/notes-backend/util/db.js
+++ part13/notes-backend/util/db.js
@@ -4,33 +4,6 @@ const { Umzug, SequelizeStorage } = require('umzug')
 
 const sequelize = new Sequelize(DATABASE_URL)
 
-// automates the process of keeping your DB schema synchronized with your
-//application's models and requirements.
-const runMigrations = async () => {
-  //umzug instance -> handles the execution, logging, and tracking of migrations
-  const migrator = new Umzug({
-    // where to find the migration scripts that define how to update the database schema
-    migrations: {
-      glob: 'migrations/*.js',
-    },/*records which migrations have been run, allowing Umzug to determine which
-     migrations need to be applied and preventing the same migration from being run
-      multiple times
-    */
-    storage: new SequelizeStorage({ sequelize, tableName: 'migrations' }),
-    /*object provides a set of methods for performing database schema operations, 
-    allowing your migration scripts to interact with the database.
-    */
-    context: sequelize.getQueryInterface(),
-    logger: console,
-  })
-  
-  //migrator.up() -> apply all pending migrations
-  const migrations = await migrator.up()
-  console.log('Migrations up to date', {
-    files: migrations.map((mig) => mig.name),
-  })
-}
-
 const connectToDatabase = async () => {
   try {
     await sequelize.authenticate()
@@ -44,4 +17,28 @@ const connectToDatabase = async () => {
   return null
 }
 
-module.exports = { connectToDatabase, sequelize }
+const migrationConf = {
+  migrations: {
+    glob: 'migrations/*.js',
+  },
+  storage: new SequelizeStorage({ sequelize, tableName: 'migrations' }),
+  context: sequelize.getQueryInterface(),
+  logger: console,
+}
+
+const runMigrations = async () => {
+  const migrator = new Umzug(migrationConf)
+  const migrations = await migrator.up()
+  console.log('Migrations up to date', {
+    files: migrations.map((mig) => mig.name),
+  })
+}
+
+const rollbackMigration = async () => {
+  await sequelize.authenticate()
+  const migrator = new Umzug(migrationConf)
+  await migrator.down()
+}
+
+
+module.exports = { connectToDatabase, sequelize, rollbackMigration }
