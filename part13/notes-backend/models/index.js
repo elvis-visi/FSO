d62384b17d5939f8ae07eebb7f8639de0297870c
elvis-visi <sivisevi1@gmail.com>
2024-03-13 10:58:40 +0100

    Synchronize DB schema by using migrations

--- part13/notes-backend/models/index.js
+++ part13/notes-backend/models/index.js
@@ -4,8 +4,7 @@ const User = require('./user')
 // 1-M relationship connection between the users and notes entries. 
 User.hasMany(Note)
 Note.belongsTo(User)
-Note.sync({ alter: true })
-User.sync({ alter: true })
+
 
 module.exports = {
   Note, User
--- part13/notes-backend/util/db.js
+++ part13/notes-backend/util/db.js
@@ -1,11 +1,40 @@
 const Sequelize = require('sequelize')
 const { DATABASE_URL } = require('./config')
+const { Umzug, SequelizeStorage } = require('umzug')
 
 const sequelize = new Sequelize(DATABASE_URL)
+
+// automates the process of keeping your DB schema synchronized with your
+//application's models and requirements.
+const runMigrations = async () => {
+  //umzug instance -> handles the execution, logging, and tracking of migrations
+  const migrator = new Umzug({
+    // where to find the migration scripts that define how to update the database schema
+    migrations: {
+      glob: 'migrations/*.js',
+    },/*records which migrations have been run, allowing Umzug to determine which
+     migrations need to be applied and preventing the same migration from being run
+      multiple times
+    */
+    storage: new SequelizeStorage({ sequelize, tableName: 'migrations' }),
+    /*object provides a set of methods for performing database schema operations, 
+    allowing your migration scripts to interact with the database.
+    */
+    context: sequelize.getQueryInterface(),
+    logger: console,
+  })
+  
+  //migrator.up() -> apply all pending migrations
+  const migrations = await migrator.up()
+  console.log('Migrations up to date', {
+    files: migrations.map((mig) => mig.name),
+  })
+}
 
 const connectToDatabase = async () => {
   try {
     await sequelize.authenticate()
+    await runMigrations()
     console.log('connected to the database')
   } catch (err) {
     console.log('failed to connect to the database')
