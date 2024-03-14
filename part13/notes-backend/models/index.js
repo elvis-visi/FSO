33696d6bafd6a7423ea4e31395bf8676cfa68714
elvis-visi <sivisevi1@gmail.com>
2024-03-14 10:16:59 +0100

    Each user can be part of a team, each team can have many users; added migration and models for teams and memberships

--- part13/notes-backend/models/index.js
+++ part13/notes-backend/models/index.js
@@ -1,11 +1,15 @@
 const Note = require('./note')
 const User = require('./user')
+const Team = require('./team')
+const Membership = require('./membership')
 
 // 1-M relationship connection between the users and notes entries. 
 User.hasMany(Note)
 Note.belongsTo(User)
 
+User.belongsToMany(Team, { through: Membership })
+Team.belongsToMany(User, { through: Membership })
 
 module.exports = {
-  Note, User
+  Note, User, Team, Membership
 }
