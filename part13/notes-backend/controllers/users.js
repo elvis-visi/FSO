9582ca412184e6367a6ed775e3a5b7795361b759
elvis-visi <sivisevi1@gmail.com>
2024-03-15 11:07:48 +0100

    Users mark notes created by other users

--- part13/notes-backend/controllers/users.js
+++ part13/notes-backend/controllers/users.js
@@ -26,7 +26,7 @@ router.put('/:username', tokenExtractor, isAdmin, async (req, res) => {
     res.status(404).end()
   }
 })
-
+//teams, their own notes, and other notes marked by the user
 router.get('/', async (req, res) => {
   const users = await User.findAll({
     include: [
@@ -60,16 +60,35 @@ router.post('/', async (req, res) => {
 
 router.get('/:id', async (req, res) => {
   const user = await User.findByPk(req.params.id, {
-    include: {
-      model: Note
-    }
+    attributes: { exclude: [''] } ,
+    include:[{
+        model: Note,
+        attributes: { exclude: ['userId'] }
+      },
+      {
+        model: Note,
+        as: 'marked_notes',
+        attributes: { exclude: ['userId']},
+        through: {
+          attributes: []
+        },
+        include: {
+          model: User,
+          attributes: ['name']
+        }
+      },
+      {
+        model: Team,
+        attributes: ['name', 'id'],
+        through: {
+          attributes: []
+        }
+      },
+    ]
   })
+
   if (user) {
-    res.json({
-      username: user.username,
-      name: user.name,
-      note_count: user.notes.length
-    })
+    res.json(user)
   } else {
     res.status(404).end()
   }
--- part13/notes-backend/models/index.js
+++ part13/notes-backend/models/index.js
@@ -2,6 +2,7 @@ const Note = require('./note')
 const User = require('./user')
 const Team = require('./team')
 const Membership = require('./membership')
+const UserNotes = require('./user_notes')
 
 // 1-M relationship connection between the users and notes entries. 
 User.hasMany(Note)
@@ -10,6 +11,9 @@ Note.belongsTo(User)
 User.belongsToMany(Team, { through: Membership })
 Team.belongsToMany(User, { through: Membership })
 
+User.belongsToMany(Note, { through: UserNotes, as: 'marked_notes' })
+Note.belongsToMany(User, { through: UserNotes, as: 'users_marked' })
+
 module.exports = {
-  Note, User, Team, Membership
+  Note, User, Team, Membership, UserNotes
 }
