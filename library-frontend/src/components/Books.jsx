846f50ffa7b1f2a00f0ab651aa9e264a01381c16
elvis-visi <sivisevi1@gmail.com>
2024-05-17 14:40:29 +0200

    Implement a Books view to show on a page all other details of all books except their genres.

--- library-frontend/src/components/Books.jsx
+++ library-frontend/src/components/Books.jsx
@@ -1,9 +1,18 @@
+import { useQuery } from "@apollo/client"
+import { ALL_BOOKS } from "../queries"
+
 const Books = (props) => {
   if (!props.show) {
     return null
   }
 
-  const books = []
+  const result = useQuery(ALL_BOOKS)
+
+  if(result.loading){
+   return <div>loading...</div> 
+  }
+
+  const books = result.data.allBooks
 
   return (
     <div>
